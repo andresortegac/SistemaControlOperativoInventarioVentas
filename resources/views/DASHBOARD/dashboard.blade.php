caadede81310b61db1490c1384cfff4c0caca291
andresortegac <andres.ortegac97@gmail.com>
2026-02-03 13:00:22 -0500

    Login estructura mvc

---
+++ resources/views/DASHBOARD/dashboard.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard')
+@section('header', 'Dashboard General')
+
+@section('content')
+<div class="dashboard-cards">
+    <div class="card">
+        <h2>{{ $totalProductos }}</h2>
+        <p>Total Productos</p>
+    </div>
+
+     <div class="card">
+        <h2>{{ $totalMovimientos }}</h2>
+        <p>Total Movimientos</p>
+    </div>    
+
+    <div class="card warning">
+        <h2>{{ $sinStock }}</h2>
+        <p>Sin Stock</p>
+    </div>
+
+    <div class="card danger">
+        <h2>{{ $stockBajo }}</h2>
+        <p>Stock Bajo</p>
+    </div>
+</div>
+
+<div class="alertas">
+    <h3>Alertas de Stock</h3>
+
+    <div class="alertas-botones">
+        <a href="{{ route('dashboard') }}" class="btn btn-primary">
+            Actualizar Dashboard
+        </a>
+
+        <a href="{{ route('productos.alertas') }}" class="btn btn-warning">
+            Ver Alertas de Stock
+        </a>
+
+        <a href="{{ route('stock.index') }}" class="btn btn-success">
+            Ver Stock Actual
+        </a>
+
+    </div>
+</div>
+  
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| LOGIN
+|--------------------------------------------------------------------------
+*/
+Route::controller(LoginController::class)->group(function () {
+
+    Route::get('/', 'show')
+        ->name('login');
+
+    Route::post('/login', 'login')
+        ->name('login.post');
+
+    Route::post('/logout', 'logout')
+        ->name('logout');
 });
